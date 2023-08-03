2f75117895aafc8f7d72beb8995c6d93e6f45728
Rene Glasow <rene.glasow@gmail.com>
2023-08-03 07:01:41 +0200

    - Navbar and SignupForm

---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarToggler,
+    MDBCollapse,
+    MDBDropdown,
+    MDBDropdownMenu,
+    MDBDropdownToggle,
+    MDBDropdownItem,
+    MDBIcon,
+    MDBContainer
+} from 'mdb-react-ui-kit';
+
+function Navbar() {
+  const [showNavSecond, setShowNavSecond] = React.useState(false);
+
+  return (
+    <Router>
+      <MDBNavbar expand='lg' light bgColor='light'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand href='#'>Asset Management</MDBNavbarBrand>
+          <MDBNavbarToggler
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+            onClick={() => setShowNavSecond(!showNavSecond)}
+          >
+            <MDBIcon icon='bars' fas />
+          </MDBNavbarToggler>
+
+          <MDBCollapse navbar show={showNavSecond}>
+            <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+              {/* Hier können weitere Navbar Elemente platziert werden */}
+            </MDBNavbarNav>
+
+            <MDBNavbarNav right fullWidth={false} className='mb-2 mb-lg-0'>
+              <MDBNavbarItem>
+                <MDBDropdown>
+                  <MDBDropdownToggle tag='a' className='nav-link'>
+                    <MDBIcon fas icon='user' />
+                  </MDBDropdownToggle>
+                  <MDBDropdownMenu>
+                      <MDBDropdownItem link href="/account">Account</MDBDropdownItem>
+                      <MDBDropdownItem link href="/license">License</MDBDropdownItem>
+                      <MDBDropdownItem link href="/logout">Logout</MDBDropdownItem>
+                  </MDBDropdownMenu>
+                </MDBDropdown>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </Router>
+  );
+}
+
+export default Navbar;
---
+++ src/components/SignUp.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { MDBBtn, MDBContainer, MDBCard, MDBCardBody, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
+import './SignUp.css'
+
+function SignUp() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Überprüfen, ob die Passwörter übereinstimmen
+    if (password !== repeatPassword) {
+      alert("Passwörter stimmen nicht überein");
+      return;
+    }
+
+    // Senden der POST-Anfrage zur Anmeldung
+    fetch('http://localhost:8000/accounts/signup/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    })
+      .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP status " + response.status);
+        }
+         return response.text();  // Ersetze response.json() durch response.text()
+      })
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+ return (
+     <form onSubmit={handleSubmit}>
+       <MDBContainer fluid className='d-flex align-items-center justify-content-center bg-image' style={{backgroundImage: 'url(https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp)'}}>
+      <div className='mask gradient-custom-3'></div>
+      <MDBCard className='m-5' style={{maxWidth: '600px'}}>
+        <MDBCardBody className='px-5'>
+          <h2 className="text-uppercase text-center mb-5">Create an account</h2>
+            <MDBInput wrapperClass='mb-4' label='Your Name' size='lg' id='form1' type='text' value={username} onChange={e => setUsername(e.target.value)}/>
+            <MDBInput wrapperClass='mb-4' label='Your Email' size='lg' id='form2' type='email' value={email} onChange={e => setEmail(e.target.value)}/>
+            <MDBInput wrapperClass='mb-4' label='Password' size='lg' id='form3' type='password' value={password} onChange={e => setPassword(e.target.value)}/>
+            <MDBInput wrapperClass='mb-4' label='Repeat your password' size='lg' id='form4' type='password' value={repeatPassword} onChange={e => setRepeatPassword(e.target.value)}/>
+
+          <div className='d-flex flex-row justify-content-center mb-4'>
+            <MDBCheckbox name='flexCheck' id='flexCheckDefault' label='I agree all statements in Terms of service' />
+          </div>
+           <MDBBtn type="submit" className='mb-4 w-100 gradient-custom-4' size='lg'>Register</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBContainer>
+     </form>
+
+  );
+}
+
+export default SignUp;
+
